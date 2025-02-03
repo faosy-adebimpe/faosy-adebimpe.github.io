43fec28835689e3cf405fa47c4d6a74f9623e7a5
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-02-03 08:08:15 -0800

    comment out ToggleButton.tsx

--- src/components/xtra/ToggleButton.tsx
+++ src/components/xtra/ToggleButton.tsx
@@ -1,29 +1,29 @@
-import { useEffect } from 'react';
-import { useStore } from '../../store';
+// import { useEffect } from 'react';
+// import { useStore } from '../../store';
 
-const ToggleButton: React.FC = () => {
-    const { darkMode, isDarkMode, setDarkMode } = useStore((store) => store);
+// const ToggleButton: React.FC = () => {
+//     const { darkMode, isDarkMode, setDarkMode } = useStore((store) => store);
 
-    useEffect(() => {
-        isDarkMode();
-    }, [isDarkMode]);
+//     useEffect(() => {
+//         isDarkMode();
+//     }, [isDarkMode]);
 
-    return (
-        <div
-            className={
-                `w-[33px] h-[19px] rounded-[41px] flex items-center justify-start p-1 bg-(--white-2) transition duration-300 ` +
-                (darkMode ? 'bg-white' : '')
-            }
-        >
-            <button
-                className={
-                    `toggle-button w-[10px] h-[10px] rounded-full bg-(--gray-1) transition duration-300 cursor-pointer ` +
-                    (darkMode ? 'active' : '')
-                }
-                onClick={setDarkMode}
-            ></button>
-        </div>
-    );
-};
+//     return (
+//         <div
+//             className={
+//                 `w-[33px] h-[19px] rounded-[41px] flex items-center justify-start p-1 bg-(--white-2) transition duration-300 ` +
+//                 (darkMode ? 'bg-white' : '')
+//             }
+//         >
+//             <button
+//                 className={
+//                     `toggle-button w-[10px] h-[10px] rounded-full bg-(--gray-1) transition duration-300 cursor-pointer ` +
+//                     (darkMode ? 'active' : '')
+//                 }
+//                 onClick={setDarkMode}
+//             ></button>
+//         </div>
+//     );
+// };
 
-export default ToggleButton;
+// export default ToggleButton;
