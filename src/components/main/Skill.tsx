68473ec904c5143441c1b1d5927030d8f6f7a6a1
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-02-04 06:47:20 -0800

    change title font to semibold, decrease the project proficiency width for small devices

--- src/components/main/Skill.tsx
+++ src/components/main/Skill.tsx
@@ -3,10 +3,10 @@ import { SkillType } from '../../types';
 const Skill: React.FC<SkillType> = ({ name, proficiency }) => {
     return (
         <div className='flex gap-[20px] justify-between items-center text-(--gray-1)'>
-            <h5 className='w-[20px] text-[14px] md:text-[16px] md:w-[25px]lg:w-[50px]'>
+            <h5 className='w-[20px] text-[12px] sm:text-[14px] md:text-[16px] md:w-[25px]lg:w-[50px]'>
                 {name}
             </h5>
-            <div className='w-[224px] h-[15.97px] bg-(--gray-1) sm:h-[18.94px] md:w-[430px] md:h-[32px] lg:w-[450px]'>
+            <div className='w-[190px] sm:w-[224px] h-[15.97px] bg-(--gray-1) sm:h-[18.94px] md:w-[430px] md:h-[32px] lg:w-[450px]'>
                 <div
                     className={`h-full w-[${proficiency}%] bg-(--white-2) text-[9px] center-center text-(--primary-color) md:text-[12px]`}
                 >
