5820c1565f45e084a4f18a879f2350bb42775252
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-02-05 16:07:31 -0800

    make nav logo clickable

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -35,9 +35,9 @@ const Navbar: React.FC = () => {
                 id='nav'
             >
                 <div className='mx-auto w-[90%] px-[2rem] h-full flex items-center justify-between'>
-                    <div className='text-[30px] md:text-[40px]'>
+                    <a href='#' className='text-[30px] md:text-[40px]'>
                         <LogoIcon />
-                    </div>
+                    </a>
                     <NavLinks />
                     <button
                         className='text-white w-10 h-10 rounded-full transition-all hover:bg-gray-500/30 flex justify-center items-center cursor-pointer text-[1.3em] md:text-[1.5em] active:scale-95 lg:hidden'
